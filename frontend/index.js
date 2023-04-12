c47f1c52783fae92a6336dddfd84021930358d38
Anisa Majhi <anisamajhi@berkeley.edu>
2023-04-12 16:34:33 -0700

    initial

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './Component';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Component className="CS70" professor="Rao"/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
