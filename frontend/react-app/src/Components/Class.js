4db4a0869cd84404f569fc8bf1dbc29c27dfaf44
Anshul2 <anshul.jambula@gmail.com>
2023-04-16 13:50:48 -0700

    updated

--- frontend/react-app/src/Components/Class.js
+++ frontend/react-app/src/Components/Class.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Button from './Button';
 import Review from './Review'
+import boxDesign from '../Styles/Class.css';
 
 function ClassRect({ className, professor, description}) {
   return (
-    <div style={{ 
-      width: '400px', 
-      height: '200px', 
-      border: '1px solid black', 
-      padding: '10px' 
-    }}>
+    <div style = {{width: '400px', 
+    height: '200px',
+    border: '1px solid black', 
+    padding: '10px'}} >
       <h2>Class: {className}</h2>
       <p>Professor: {professor}</p>
       <p>Class Description:{description}</p>
